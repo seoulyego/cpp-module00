0b863d95ec51e291f03f851756627b56ec16f829
yeongo <yeongo@student.42seoul.kr>
2023-07-13 23:40:23 +0900

    feat: ing...  PhoneBook::search_contact

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,3 +1,10 @@
 #include "PhoneBook.hpp"
 
-void	PhoneBook::add_contact(void);
+void	PhoneBook::add_contact(void) {
+	this->contacts[index % 8].add_contact();
+	this->index++;
+}
+
+void	PhoneBook::search_contact(void) {
+	;
+}
